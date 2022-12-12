c8510c6cc63847b37433c81da2121a60b13bc9c3
nervinjf <nervinjflores@gmail.com>
2022-12-12 14:19:53 -0400

    Se actualizo

--- src/services/cita.services.js
+++ src/services/cita.services.js
@@ -39,33 +39,33 @@ class CitaServices {
     static async register(newC){
         try {
             const result = await CitaCoti.create(newC);
-            const result2 = await CitaCoti.findOne({
-                where: {id: result.toamdorId},
-                attributes: ["codigo", "fecha", "tipo", "plan", "asegurados", "fPago", "efectivo", "tiempo", 
-            "fCliente", "fDevolucion", "adjunto", "poliza", "statusSuscripcion", "createdAt"],
-                include:[
-                    {
-                       model: Tomador,
-                    as: "tomador",
-                    attributes: [
-                        "firstname",
-                        "lastname",
-                        "ci",
-                        "email",
-                        "phone1"
-                    ], 
-                    },
-                    {
-                        model: Users,
-                        as: "Users",
-                        attributes: [
-                            "firstname",
-                            "lastname",
-                            "id"
-                        ], 
-                    }
-                ],
-            });
+            // const result2 = await CitaCoti.findOne({
+            //     where: {id: result.toamdorId},
+            //     attributes: ["codigo", "fecha", "tipo", "plan", "asegurados", "fPago", "efectivo", "tiempo", 
+            // "fCliente", "fDevolucion", "adjunto", "poliza", "statusSuscripcion", "createdAt"],
+            //     include:[
+            //         {
+            //            model: Tomador,
+            //         as: "tomador",
+            //         attributes: [
+            //             "firstname",
+            //             "lastname",
+            //             "ci",
+            //             "email",
+            //             "phone1"
+            //         ], 
+            //         },
+            //         {
+            //             model: Users,
+            //             as: "Users",
+            //             attributes: [
+            //                 "firstname",
+            //                 "lastname",
+            //                 "id"
+            //             ], 
+            //         }
+            //     ],
+            // });
 
             const result3 = {...result2, ...result}
             console.log(result3)
@@ -77,40 +77,40 @@ class CitaServices {
         }
     }
 
-    static async findOne(id){
-        try {
-            const result = await CitaCoti.findOne({
-                where: {id},
-                attributes: ["codigo", "fecha", "tipo", "plan", "asegurados", "fPago", "efectivo", "tiempo", 
-            "fCliente", "fDevolucion", "adjunto", "poliza", "statusSuscripcion", "createdAt"],
-                include:[
-                    {
-                       model: Tomador,
-                    as: "tomador",
-                    attributes: [
-                        "firstname",
-                        "lastname",
-                        "ci",
-                        "email",
-                        "phone1"
-                    ], 
-                    },
-                    {
-                        model: Users,
-                        as: "Users",
-                        attributes: [
-                            "firstname",
-                            "lastname",
-                            "id"
-                        ], 
-                    }
-                ],
-            });
-            return result;
-        } catch (error) {
-            throw error;
-        }
-    }
+    // static async findOne(id){
+    //     try {
+    //         const result = await CitaCoti.findOne({
+    //             where: {id},
+    //             attributes: ["codigo", "fecha", "tipo", "plan", "asegurados", "fPago", "efectivo", "tiempo", 
+    //         "fCliente", "fDevolucion", "adjunto", "poliza", "statusSuscripcion", "createdAt"],
+    //             include:[
+    //                 {
+    //                    model: Tomador,
+    //                 as: "tomador",
+    //                 attributes: [
+    //                     "firstname",
+    //                     "lastname",
+    //                     "ci",
+    //                     "email",
+    //                     "phone1"
+    //                 ], 
+    //                 },
+    //                 {
+    //                     model: Users,
+    //                     as: "Users",
+    //                     attributes: [
+    //                         "firstname",
+    //                         "lastname",
+    //                         "id"
+    //                     ], 
+    //                 }
+    //             ],
+    //         });
+    //         return result;
+    //     } catch (error) {
+    //         throw error;
+    //     }
+    // }
 }
 
 module.exports = CitaServices;
