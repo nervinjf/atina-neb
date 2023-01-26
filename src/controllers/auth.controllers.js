d7b8bf57a2390dff247bd202c210706e2a51e3c5
nervinjf <nervinjflores@gmail.com>
2023-01-26 14:43:26 -0400

    Se actualizo

--- src/controllers/auth.controllers.js
+++ src/controllers/auth.controllers.js
@@ -1,30 +1,33 @@
-const {AuthServices} = require("../services");
+const { AuthServices } = require("../services");
 
 const userLogin = async (req, res, next) => {
-    try {
-        const credentials = req.body;
-        const result = await AuthServices.authenticate(credentials);
-
-        if (result) {
-            const { username, email, id } = result.result;
-            const user = { username, email, id };
-
-            const token = AuthServices.getToken(user);
-            user.token = token;
-            res.json({...user});
-        } else {
-            res.status(400).json({ message: "Información inválida" });
-        }
-
-    } catch (error) {
-        next({
-            status: 400,
-            errorContent: error,
-            message: "Email o contraseña inválida",
-          });
+  try {
+    // email y password
+    const credentials = req.body;
+    const result = await AuthServices.authenticate(credentials);
+    // false --> no era contraseña invalida
+    // null --> no se encuentra al usuario
+    // { isValid, result }
+    if (result) {
+      const { username, email, id } = result.result;
+      const user = { username, email, id };
+      // console.log(user)
+      const token = AuthServices.genToken(user);
+      user.token = token;
+      res.json({ ...user });
+      console.log({ ...user })
+    } else {
+      res.status(400).json({ message: "Información inválida" });
     }
-}
+  } catch (error) {
+    next({
+      status: 400,
+      errorContent: error,
+      message: "Email o contraseña inválida",
+    });
+  }
+};
 
 module.exports = {
-    userLogin
-} 
+  userLogin,
+};
