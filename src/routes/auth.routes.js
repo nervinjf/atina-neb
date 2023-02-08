0acc20e3a50652d6193f16793795e3cbd1aade6b
nervinjf <109636023+nervinjf@users.noreply.github.com>
2023-02-08 10:50:52 -0400

    Delete src directory

--- src/routes/auth.routes.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express");
-const { userLogin } = require("../controllers");
-
-const router = Router();
-
-router.post("/auth/login", userLogin);
-
-module.exports = router;
