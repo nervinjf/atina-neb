ab0ff2d04b982c05bb6babd838300dd6a01c5030
nervinjf <nervinjflores@gmail.com>
2023-03-12 14:27:42 -0400

    Actualizacion Raelway

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,8 +1,9 @@
 const { Router } = require("express");
 const { userLogin } = require("../controllers");
+const { cors } = require('cors')
 
 const router = Router();
 
-router.post("/auth/login", userLogin);
+router.post("/auth/login",  cors({origin: "*"}),userLogin);
 
 module.exports = router;
